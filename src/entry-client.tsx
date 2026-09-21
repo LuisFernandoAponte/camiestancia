23dbc42914ab231008b780d5a0e4319bef5ca083
Luis Fernando Aponte <luisfer.aponte191998@gmail.com>
2026-09-21 18:26:33 -0400

    fix(client): append root div dynamically if not present

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -3,12 +3,16 @@ import { RouterProvider } from "@tanstack/react-router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { router, queryClient } from "./router";
 
-const rootElement = document.getElementById("root");
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} context={{ queryClient }} />
-    </QueryClientProvider>
-  );
+let rootElement = document.getElementById("root");
+if (!rootElement) {
+  rootElement = document.createElement("div");
+  rootElement.id = "root";
+  document.body.appendChild(rootElement);
 }
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} context={{ queryClient }} />
+  </QueryClientProvider>
+);
